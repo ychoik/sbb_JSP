fdf573112862f4a86f7d75b439ad2c2aca82b032
Chan Ik Yoon <ychoik0818@naver.com>
2026-05-13 01:47:52 +0900

    first commit

---
+++ src/test/java/org/example/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
